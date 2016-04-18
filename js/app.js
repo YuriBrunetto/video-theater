a53b19b75c8dcb27fe34ff42e79d2106de51b0df
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-04-17 23:52:31 -0300

    alpha version ready

--- js/app.js
+++ js/app.js
@@ -11,18 +11,25 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
             if ($overlay.length === 0) { // if there isn't already a .overlay div in the <body>
                 if ($video.length >= 1) { // if there's a <video> in the body
-                    console.log($video.length, "entrou3");
+                    var video_url = window.location.href;
+                    video_url = video_url.split("=");
+                    video_url = video_url[1];
+
+                    console.log(video_url);
+
                     $body.append(_overlay);
+                    $video.remove();
+
+                    // TODO: use YouTube API
+                    var $new_video = "<iframe class='video-theater' src='https://www.youtube.com/embed/"+ video_url +"?rel=0&autoplay=1' frameborder='0' allowfullscreen></iframe>";
+
+                    $(".overlay").html($new_video);
                 } else {
 
                 }
             }
         }
 
-        $(function(){
-
-            VideoTheater.init();
-
-        });
+        $(function(){ VideoTheater.init(); });
     }
 });
